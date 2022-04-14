bf8a12149a3f938a9bf219846e0a5031f3dc5c6d
khacngocptit <khacngocit@gmail.com>
2022-04-14 12:35:27 +0700

    Fix bug danh muc thuc pham

--- src/modules/san-pham/entities/san-pham.entity.ts
+++ src/modules/san-pham/entities/san-pham.entity.ts
@@ -1,6 +1,7 @@
-import { Schema, SchemaFactory } from "@nestjs/mongoose";
-import { DB_SAN_PHAM } from "src/modules/repository/db-collection";
+import { Prop, raw, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { DB_CUA_HANG, DB_SAN_PHAM } from "src/modules/repository/db-collection";
 import * as mongoose from "mongoose";
+import { IsNumber, IsOptional, IsString } from "class-validator";
 
 @Schema({
     collection: DB_SAN_PHAM,
@@ -8,6 +9,33 @@ import * as mongoose from "mongoose";
 
 export class SanPham {
 
+    @IsString()
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: DB_CUA_HANG })
+    cuaHangId: string;
+
+    @IsString()
+    @Prop()
+    tenSanPham: string;
+
+    @Prop()
+    maSanPham: string;
+
+    @IsString({ each: true })
+    @Prop(raw([String]))
+    anhSanPham: string[];
+
+    @IsString()
+    @IsOptional()
+    @Prop()
+    moTa?: string;
+
+    @IsNumber()
+    @Prop()
+    soLuongSanPham: string;
+
+    @IsNumber()
+    @Prop()
+    giaTien: string;
 }
 
 export const SanPhamSchema = SchemaFactory.createForClass(SanPham);
