0a7e1f1a31e2afd4b48ab2e69ab14ff5418191a7
khacngocptit <khacngocit@gmail.com>
2022-04-24 15:02:12 +0700

    push

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -2,6 +2,5 @@ import { PickType } from "@nestjs/swagger";
 import { CreateProfileDto } from "../../profile/dto/create-profile.dto";
 import { User } from "../entities/user.entity";
 
-export class CreateUserDto extends PickType(User, ["username", "password", "email"]) {
-    profile: CreateProfileDto;
-}
+export class CreateUserDto extends User { }
+
