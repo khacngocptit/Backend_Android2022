63b2c6a30f298b143c8900244e16853170045072
khacngocptit <khacngocit@gmail.com>
2022-04-24 22:27:21 +0700

    push

--- src/modules/cua-hang/cua-hang.module.ts
+++ src/modules/cua-hang/cua-hang.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { CuaHangController } from './cua-hang.controller';
-import { CuaHangService } from './cua-hang.service';
+import { Module } from "@nestjs/common";
+import { CuaHangController } from "./cua-hang.controller";
+import { CuaHangService } from "./cua-hang.service";
 
 @Module({
   controllers: [CuaHangController],
-  providers: [CuaHangService]
+  providers: [CuaHangService],
 })
-export class CuaHangModule {}
+export class CuaHangModule { }
